777be2155a53652e1edfc2a2086cad56814b7087
EzraChai <juanzhe2@gmail.com>
2020-10-30 19:06:28 +0800

    ASK

--- PythonAutoGUI.py
+++ PythonAutoGUI.py
@@ -1,13 +1,20 @@
 import pyautogui,time,keyboard
 word="Hello"
 
+print("Auto Typing Hello System is starting in ")
+
+for x in range (1,6):
+    print(x)
+    time.sleep(1)
+
+print("\nPress backspace for 1 second to stop the System.")
+
 def typeHello():
         pyautogui.typewrite(word)
         pyautogui.press("enter")
         time.sleep(1)
 
-time.sleep(5)
 while 1:
-    if keyboard.is_pressed("q"):
+    if keyboard.is_pressed("backspace"):
         exit(0)
     typeHello()
