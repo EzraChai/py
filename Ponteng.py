12a6534b18d42661c165f762d549348f16d30f54
EzraChai <juanzhe2@gmail.com>
2020-11-02 21:49:52 +0800

    ASK

--- Ponteng.py
+++ Ponteng.py
@@ -2,21 +2,21 @@ kekerapan = 0
 
 flag = str(input("Adakah anda pernah ponteng sekolah? [Ya / Tidak]: "))
 
-while ((flag.lower()) not in ["ya","tidak"]):
+while (flag.lower() not in ["ya","tidak"]):
     flag = str(input("Adakah anda pernah ponteng sekolah? [Ya / Tidak]: "))
 else:
     if flag.lower() == "ya":
         kekerapan = int(input("Masukkan bilangan hari anda telah ponteng: "))
         if kekerapan <= 1:
-            print(" Anda akan dikenakan hukuman dengan dirotan sekali.") 
+            print("\nAnda akan dikenakan hukuman dengan dirotan sekali.") 
         elif kekerapan <= 3:
-            print(" Anda akan dikenakan hukuman dengan dirotandua kali.") 
+            print("\nAnda akan dikenakan hukuman dengan dirotan dua kali.") 
         elif kekerapan <= 5:
-            print("Anda akan dikenakan hukuman dengan digantung sekolah selama seminggu.")
+            print("\nAnda akan dikenakan hukuman dengan digantung sekolah selama seminggu.")
         else:
-            print(" Anda akan dikenakan hukuman dengan dengan dibuang sekolah.")
+            print("\nAnda akan dikenakan hukuman dengan dengan dibuang sekolah.")
     else:
-        print("Anda seorang pelajar yang berdisiplin.")    
+        print("\nAnda seorang pelajar yang berdisiplin.")    
       
     
 print("\nTerima kasih atas kerjasama anda")              
