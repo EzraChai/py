1cc718ba35e33e4684e67bb44bc892ced1b2eee5
EzraChai <juanzhe2@gmail.com>
2020-11-05 16:30:17 +0800

    ASK

---
+++ AutoFacebookLogin.py
@@ -0,0 +1,24 @@
+from msedge.selenium_tools import options
+from msedge.selenium_tools import Edge,EdgeOptions
+from time import sleep
+
+options = EdgeOptions()
+options.use_chromium = True 
+driver = Edge(options=options)
+
+usr = "juanzhe2@gmail.com"
+pwd = "D9806f2a2b"
+
+driver.get("https://www.facebook.com/")
+print("Opened Facebook")
+sleep(0.5)
+
+username_box = driver.find_element_by_id("email")
+username_box.send_keys(usr)
+print("Email ID entered")
+sleep(0.1)
+
+password_box = driver.find_element_by_id("pass")
+password_box.send_keys(pwd)
+print("Password entered")
+sleep(0.3)
---
+++ DriverTest.py
@@ -0,0 +1,7 @@
+from msedge.selenium_tools import Edge,EdgeOptions
+
+options = EdgeOptions()
+options.use_chromium = True 
+driver = Edge(options=options)
+
+driver.get("https://facebook.com/")
